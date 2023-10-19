b9b8b3216e076581a4775dbef68a81da96062602
Chamfrado <prog.lohran@gmail.com>
2023-10-19 16:05:07 -0300

    Fazendo as pesquisas

--- sacfullnet/Backend/src/main/java/br/com/sacfullnet/sacfullnet/service/impl/EquipmentServiceImpl.java
+++ sacfullnet/Backend/src/main/java/br/com/sacfullnet/sacfullnet/service/impl/EquipmentServiceImpl.java
@@ -6,38 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.sacfullnet.sacfullnet.model.Equipamento;
-import br.com.sacfullnet.sacfullnet.repository.dao.EquipamentoDao;
+import br.com.sacfullnet.sacfullnet.repository.dao.EquipmentDao;
 import br.com.sacfullnet.sacfullnet.service.EquipamentoService;
 
 @Service
 public class EquipamentoServiceImpl implements EquipamentoService {
 
     @Autowired
-    EquipamentoDao equipamentoDao;
+    EquipmentDao equipmentDao;
 
     @Override
     public List<Equipamento> find(){
-        return equipamentoDao.find();
+        return equipmentDao.find();
     }
 
     @Override
     public Equipamento findById(int id){
-        return equipamentoDao.findById(id);
+        return equipmentDao.findById(id);
     }
 
     @Override
     public int save(Equipamento user){
-        return equipamentoDao.save(user);
+        return equipmentDao.save(user);
     }
 
     @Override
     public boolean update(Equipamento user){
-        return equipamentoDao.update(user);
+        return equipmentDao.update(user);
     }
 
     @Override
     public boolean delete(int id){
-        return equipamentoDao.delete(id);
+        return equipmentDao.delete(id);
     }
     
 }
